fa97458028271a0c8d1049a0b0332ed62782edc4
vrajnandak <vrajnandaknangunoori@gmail.com>
2025-11-25 16:22:54 +0530

    Model Api integrated

---
+++ src/api.py
@@ -0,0 +1,68 @@
+import io
+import numpy as np
+from flask import Flask, request, jsonify
+from PIL import Image
+import mlflow
+import mlflow.tensorflow
+import tensorflow as tf
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+# SET TRACKING URI (VERY IMPORTANT)
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Classes
+CLASSES = ['glioma', 'meningioma', 'notumor', 'pituitary']
+
+# MLflow model URI (Production)
+MODEL_URI = "models:/SPE_Brain_Tumor_Classifier/Production"
+
+print(f"🔄 Loading model from MLflow Registry: {MODEL_URI}")
+model = mlflow.tensorflow.load_model(MODEL_URI)
+print("✅ Model loaded successfully from MLflow!")
+
+IMAGE_SIZE = 224
+
+def preprocess_image(image):
+    image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+    image = image.convert("RGB")
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files["file"]
+    img_bytes = file.read()
+
+    try:
+        image = Image.open(io.BytesIO(img_bytes))
+    except:
+        return jsonify({"error": "Invalid image format"}), 400
+
+    img_array = preprocess_image(image)
+
+    preds = model.predict(img_array)
+    class_index = int(np.argmax(preds))
+    class_name = CLASSES[class_index]
+    confidence = float(np.max(preds))
+
+    return jsonify({
+        "filename": file.filename,
+        "prediction": class_name,
+        "confidence": confidence
+    })
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "HI, How are you?"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
